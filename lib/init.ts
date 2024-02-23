04b8336a75bff51f43840fac8ce2a32c94719850
Damiano <dpapi68@gmail.com>
2024-02-23 16:11:07 +0100

    fix: Logger instance

--- lib/init.ts
+++ lib/init.ts
@@ -1,11 +1,21 @@
 import { LogFlake } from "./LogFlake"
+import { IBodyLog } from "./types"
 
-export let Logger: LogFlake | null = null
+let Logger: LogFlake | null = null
 
 interface IInitOptions {
   hostname?: string
   enableCompression?: boolean
 }
+
+export const getLogger = () => {
+  return Logger
+}
+
+export const sendLog = (content: string, options?: Omit<IBodyLog, "content">) => getLogger()?.sendLog(content, options)
+export const sendException = (error: Error, options?: Pick<IBodyLog, "correlation">) => getLogger()?.sendException(error, options)
+export const measurePerformance = (label: string) => getLogger()?.measurePerformance(label)
+export const sendPerformance = (label: string, duration: number) => getLogger()?.sendPerformance(label, duration)
 
 export function initialize(appId: string, server?: string, options?: IInitOptions) {
   if (Logger !== null) {
